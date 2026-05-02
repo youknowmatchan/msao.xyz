c44301ca4919c5fd70b1dec0ace6580d20efe2b3
matchan <matchalive@protonmail.com>
2026-05-03 10:01:27 +1200

    Update +page.svelte

--- src/routes/portal/+page.svelte
+++ src/routes/portal/+page.svelte
@@ -3,7 +3,7 @@
     <p>Staff Member may use this to access and use Miskatonic University services, and update edit this website, webpages. Unauthorize acess may be punishable by law.</p>
     <p>Currently the staff portal have been moved onto your personal portal of the msaobservatory website, please remember to register and login and if you have any problem please contact the miskatonic technical team</p>
     <br>
-    <p>Example for the personal portal, msaobservatory.xyz/janedoe </p>
+    <p>Example for the personal portal, msaobservatory.xyz/jdoe for Jane Doe </p>
     
 </div>
 <div class="pt-2">
